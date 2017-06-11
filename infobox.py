689d5e02871d7f0e702f3e6cb66747c8225519a2
Jimmy Le <jldevops@users.noreply.github.com>
2017-06-11 15:04:43 -0400

    Reworked the regex implementation of each of the addresses for birth_place and death_place

--- infobox.py
+++ infobox.py
@@ -1,47 +1,84 @@
-import urllib2, json, pprint, re, datetime
+import urllib2, json, re
 import mwparserfromhell
 
-def _parseDate(wikiDate):
-  ''' Given the JSON data from InfoBox():
+
+def parse_date(wiki_date):
+    '''
+      Given the JSON data from InfoBox():
       Input: JSON Data from InfoBox() that located Birth_Date and Death_Date
       Output: Birth_Date and Death_Date in the format, MM - DD - YYYY
-  '''
-  template = mwparserfromhell.parse("%s"%wikiDate.value)
-  try:
+    '''
 
+    template = mwparserfromhell.parse("%s" % wiki_date.value)
+    # print template
     try:
-      ## This parses and separates the year, month, and day into separate entities to combine -- MM - DD - YYYY ##
-      d = map(template.filter_templates()[0].get, [1,2,3])
-      d = [int('%s'%x.value) for x in d]
-      dateFinal = str(d[1]) +"-"+ str(d[2]) +"-"+ str(d[0])
-      return dateFinal
+        try:
+            # This parses and separates the year, month, and day into separate entities to combine -- MM - DD - YYYY #
+            d = map(template.filter_templates()[0].get, [1, 2, 3])
+            d = [int('%s' % x.value) for x in d]
+            dateFinal = str(d[1]) + "-" + str(d[2]) + "-" + str(d[0])
+            return dateFinal
+        except ValueError as e:
+            # This exception is used if there is only a Year for the Person #
+            d = map(template.filter_templates()[0].get, [1])
+            d = [int('%s' % x.value) for x in d]
+            dateFinal = str(d[0])
+            return dateFinal
+
     except Exception as e:
-      ## This exception is used if there is only a Year for the Person ###
-      d = map(template.filter_templates()[0].get, [1])
-      d = [int('%s'%x.value) for x in d]
-      dateFinal = str(d[0])
-      return dateFinal
+        print "ERROR: " + wiki_date + "not found in the infobox."
+        raise e
 
-  except Exception as e:
-    tempTest = re.sub('\n', '', str(template))
-    return str(tempTest)
-  else:
-    d = None
-    return d
 
-def _parsePlace(wikiPlace):
-  ''' Given the JSON data from InfoBox():
+def parse_place(wiki_place):
+    '''
+      Given the JSON data from InfoBox():
       Input: JSON Data from InfoBox() that located Birth_Place and Death_Place
       Output: Birth_Place and Death_Place strings
-  '''
-  template = mwparserfromhell.parse("%s"%wikiPlace.value)
-  tempPhase = re.sub('[\(\)\{\}<>\[\]\|\n]', '', str(template))
-  tempPhase2 = re.sub('nowrap', '', str(tempPhase))
-  tempFinal= re.sub('br/', '', str(tempPhase2))
-  return (str(tempFinal))
+    '''
+    template = mwparserfromhell.parse("%s" % wiki_place.value)
+    try:
+        # template = re.sub('\|.*?\]', '', str(template))
+        template = re.sub('[\{\}<>\[\n]', '', str(template))
+        # template = re.sub('[\{\}<>\[\]\n]', '', str(template))
+        [town, city, country] = template.split(',')
 
-def _parseInfobox(page):
-  ''' Given the JSON data from WikiAge():
+        try:
+            town = re.sub('nowrap\|', '', town)
+            town = re.sub('\|.*?\]', '', town)
+            town = re.sub('[\(\[].*?[\)\]]', '', town)
+            town = re.sub('br/', '', town)
+        except Exception as e:
+            print "ERROR: Information regarding the person's town could not be found"
+            raise e
+
+        try:
+            city = re.sub('nowrap\|', '', city)
+            city = re.sub('\|.*?\]', '', city)
+            city = re.sub('[\(\[].*?[\)\]]', '', city)
+            city = re.sub('br/', '', city)
+        except Exception as e:
+            print "ERROR: Information regarding the person's city could not be found"
+            raise e
+        try:
+            country = re.sub('nowrap\|', '', country)
+            country = re.sub('\|.*?\]\]', '', country)
+            country = re.sub('[\(\[].*?[\)\]]', '', country)
+            country = re.sub('br/', '', country)
+        except Exception as e:
+            print "ERROR: Information regarding the person's country could not be found"
+            raise e
+
+        address = town + ", " + city + ", " + country
+        return address
+    except Exception as e:
+        print "ERROR: " + wiki_place + " could not be parsed or found in the infobox."
+        raise e
+
+
+def parse_infobox(page):
+    '''
+      Given the JSON data from WikiAge():
       Input: JSON Data from WikiAge(), locates the Infobox heading
       Output: Selected information from each specific function
 
@@ -50,78 +87,87 @@ def _parseInfobox(page):
           Key:death_date  Value: 1-8-1642
           Key:birth_place  Value:  Pisa, Duchy of Florence, Italy
           Key:name  Value:  Galileo Galilei
-  '''     
-  try:
-    code = mwparserfromhell.parse(page)
-    for template in code.filter_templates():
-      if 'Infobox' in template.name or 'infobox' in template.name:
+    '''
 
-        output = {}
+    try:
+        code = mwparserfromhell.parse(page)
+        for template in code.filter_templates():
+            if 'Infobox' in template.name or 'infobox' in template.name:
 
-        try:
-          output['name'] = "%s"%template.get('name').value
-        except ValueError as e:
-          output['name'] = "%s"%template.get('birth_name').value
-        # print "Name was found"
+                output = {}
 
-        for date in ['birth_date', 'death_date']:
-          try:
-            item = _parseDate(template.get(date))
-          except ValueError as e:
-            item = None
-          output[date] = item
+                try:
+                    try:
+                        output['name'] = "%s" % template.get('name').value
+                    except Exception as ex:
+                        output['name'] = "%s" % template.get('birth_name').value
+                except ValueError as e:
+                    print "Name was not Found"
+                    raise e
 
-        for place in ['birth_place', 'death_place']:
-          try:
-            item = _parsePlace(template.get(place))
-          except ValueError as e:
-            item = None
-          output[place] = item
-        
-        
-        return output
-        
-    raise ValueError('Missing InfoBox')
+                for date in ['birth_date', 'death_date']:
+                    try:
+                        item = parse_date(template.get(date))
+                    except ValueError as e:
+                        item = None
+                    output[date] = item
 
-  except Exception as e:
-    ## Uncommented for Now for Testing ##
-    # print "Failed to Properly Parse Infobox for Information"
-    # raise e
-    pass
+                for place in ['birth_place', 'death_place']:
+                    try:
+                        item = parse_place(template.get(place))
+                    except ValueError as e:
+                        item = None
+                    output[place] = item
+
+                return output
+
+        raise ValueError('Missing InfoBox')
+
+    except Exception as e:
+        ## Uncommented for Testing ##
+        # print "Failed to Properly Parse Infobox for Information"
+        # raise e
+        pass
 
 
-def wikiAge(wikiTitle, function=None):
-  ''' Parse a wikipedia url to run a function on the data '''
+def wiki_info(wiki_title, function=None):
+    ''' Parse a wikipedia url to run a function on the data '''
 
-  URLTEMPLATE = 'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=%s&prop=revisions&rvprop=content'
-  try:
-    pageJson = urllib2.urlopen(URLTEMPLATE%(wikiTitle)).readlines()[0]
-  except Exception as e:
-    print "Failed to Read page: %s"%(URLTEMPLATE%(wikiTitle))
-    raise e
+    url_template = 'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=%s&prop=revisions&rvprop=content'
 
-  try:
-    page = json.loads(pageJson)
-    page = page['query']['pages']
-    pageid = page.keys()[0]
-    page = page[pageid]['revisions'][0]['*'] 
-    
-    return function(page)
+    wiki_title = wiki_title.replace(" ", "_")
 
-  except Exception as e:
-    ## Uncommented for Now for Testing ##
-    # print 'Failed to Process Wikipedia Information -- Make sure the link is registering'
-    # raise e
-    pass
+    try:
+        page_json = urllib2.urlopen(url_template % wiki_title).readlines()[0]
+    except Exception as e:
+        print "ERROR: Failed to open link to Wikipedia page: %s" % (url_template % wiki_title)
+        raise e
+
+    try:
+        page = json.loads(page_json)
+        page = page['query']['pages']
+        page_id = page.keys()[0]
+        page = page[page_id]['revisions'][0]['*']
+        # print page
+        return function(page)
+    except Exception as e:
+        print 'ERROR: Failed to parse the Wikipedia information'
+        raise e
+
 
 # Test Cases to Use (Uncomment for use)
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-#   person = wikiAge('Michael_Pettis', function=_parseInfobox)
-#   try:
-#     for key in person:
-#       print 'Key:%s  Value: %s'%(key,person[key])
-#   except Exception as e:
-#     pass
+    person = wiki_info('Vladimir Putin', function=parse_infobox)
+    try:
+        for key in person:
+            print 'Key:%s  Value: %s' % (key, person[key])
+    except Exception as e:
+        pass
 
-
+    person = wiki_info('Mark_Zuckerberg', function=parse_infobox)
+    try:
+        for key in person:
+            print 'Key:%s  Value: %s' % (key, person[key])
+    except Exception as e:
+        pass
--- unit-csv.py
+++ unit-csv.py
@@ -1,18 +1,16 @@
 import unittest, csv
-from infobox import wikiAge, _parseInfobox
+from infobox import wiki_info, parse_infobox
 
 ''' CSV Reader to test the infobox.py file for a list of People's names on Wikipedia '''
 with open('names.csv', 'rb') as csvfile:
-	fileReader = csv.reader(csvfile, delimiter=',')
-	for row in fileReader:
-		person = wikiAge(row[0], function=_parseInfobox)
-		try:
-  			for key in person:
-  				print 'Key:%s  Value: %s'%(key,person[key])
-  		except Exception as e:
-  			pass
-
-
+    fileReader = csv.reader(csvfile, delimiter=',')
+    for row in fileReader:
+        person = wiki_info(row[0], function=parse_infobox)
+        try:
+            for key in person:
+                print 'Key:%s  Value: %s' % (key, person[key])
+        except Exception as e:
+            pass
 
 ''' Unit tests for the names and information '''
 ''' Not finished writing '''
@@ -20,7 +18,7 @@ with open('names.csv', 'rb') as csvfile:
 # class nameTest(unittest.TestCase):
 
 #     def testOne(self):
-#     
+#
 # def main():
 #     unittest.main()
 
