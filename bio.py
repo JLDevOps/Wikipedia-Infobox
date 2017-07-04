f07b263de16afccca5017cd4fe95a4f544c48aaf
Jimmy Le <jldevops@gmail.com>
2017-07-04 15:40:30 -0400

    Updated requirements.txt for easy install of NLTK kit for natural language processing.  Testing NLTK in bio.py file.

--- bio.py
+++ bio.py
@@ -1,6 +1,20 @@
-import urllib2, json, re
+import urllib2, json, re, nltk
 import mwparserfromhell
+from infobox import wiki_info, parse_infobox
 
 '''
     This file is for the scraping of the BIO section of the Wikipedia page
 '''
+
+if __name__ == '__main__':
+    sentence = """At eight o'clock on Thursday morning ... New York didn't feel very good."""
+    tokens = nltk.word_tokenize(sentence)
+    tagged = nltk.pos_tag(tokens)
+
+    entities = nltk.chunk.ne_chunk(tagged)
+    print(entities)
+
+
+
+
+
